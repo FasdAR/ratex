590456a70170c085efe27fae06feb24a7bef2fb5
Andrey Rednikov <andreyrednikov@gmail.com>
2020-09-17 20:34:46 +0300

    Add gradle dependencies in app

--- app/src/main/java/ru/fasdev/ratex/MainActivity.kt
+++ app/src/main/java/ru/fasdev/ratex/MainActivity.kt
@@ -3,8 +3,10 @@ package ru.fasdev.ratex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
