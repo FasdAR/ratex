b9fab6df3baf811dd3e0dba54df72e5cb98931ef
Andrey Rednikov <andreyrednikov@gmail.com>
2020-09-17 19:48:17 +0300

    Add CurrencyDomainTest

---
+++ domain/src/test/java/ru/fasdev/ratex/domain/currencyRate/entity/CurrencyDomainTest.kt
@@ -0,0 +1,31 @@
+package ru.fasdev.ratex.domain.currencyRate.entity
+
+import junit.framework.Assert.*
+import org.junit.Test
+import ru.fasdev.ratex.domain.currencyRate.entity.extension.toCurrencyDomain
+import java.util.*
+
+class CurrencyDomainTest {
+
+    @Test
+    fun getInstance()
+    {
+        val result = CurrencyDomain.getInstance("USD")
+
+        assertNotNull(result)
+        assertEquals(result.symbol, "$")
+    }
+
+    @Test
+    fun toCurrencyDomain()
+    {
+        val currency = Currency.getInstance("USD")
+        val currencyDomain = currency.toCurrencyDomain()
+
+        assertNotNull(currencyDomain)
+
+        assertEquals(currencyDomain.symbol, currency.symbol)
+        assertEquals(currencyDomain.currencyCode, currency.currencyCode)
+        assertEquals(currencyDomain.displayName, currency.displayName)
+    }
+}
