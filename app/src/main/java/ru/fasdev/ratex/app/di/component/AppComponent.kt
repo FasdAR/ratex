ee5c43754395d0c7969947a8a5f64bf663013d09
Andrey Rednikov <andreyrednikov@gmail.com>
2020-09-18 20:31:25 +0300

    Reformate di

--- app/src/main/java/ru/fasdev/ratex/app/di/component/AppComponent.kt
+++ app/src/main/java/ru/fasdev/ratex/app/di/component/AppComponent.kt
@@ -1,26 +1,12 @@
 package ru.fasdev.ratex.app.di.component
 
-import android.content.Context
-import dagger.BindsInstance
 import dagger.Component
 import ru.fasdev.ratex.app.RatexApp
+import ru.fasdev.ratex.app.di.module.AppModule
 import ru.fasdev.ratex.app.di.module.SettingsModule
-import javax.inject.Named
 
-@Component(modules = [SettingsModule::class])
+@Component(modules = [AppModule::class, SettingsModule::class])
 interface AppComponent
 {
-    @Component.Builder
-    interface Builder {
-        fun build(): AppComponent
-
-        @BindsInstance
-        fun context(context: Context): Builder
-
-        @BindsInstance
-        @Named("name_settings")
-        fun nameSettings(name: String): Builder
-    }
-
     fun inject(app: RatexApp)
 }
