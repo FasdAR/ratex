7b966d42525ec84f3da3b60f91414aaaeabb6ddd
Andrey Rednikov <andreyrednikov@gmail.com>
2020-09-25 02:54:48 +0300

    Edit behavior dialog for keyboard

--- app/src/main/java/ru/fasdev/ratex/ui/view/bottomSheetSelectCurrency/SelectCurrencyBottomSheet.kt
+++ app/src/main/java/ru/fasdev/ratex/ui/view/bottomSheetSelectCurrency/SelectCurrencyBottomSheet.kt
@@ -1,23 +1,28 @@
 package ru.fasdev.ratex.ui.view.bottomSheetSelectCurrency
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import moxy.MvpBottomSheetDialogFragment
 import moxy.ktx.moxyPresenter
 import ru.fasdev.ratex.R
 import ru.fasdev.ratex.app.di.component.DaggerSelectCurrencyBottomSheetComponent
+import ru.fasdev.ratex.app.util.dp
 import ru.fasdev.ratex.databinding.SelectCurrencyBottomSheetBinding
 import ru.fasdev.ratex.domain.currency.entity.CurrencyDomain
 import ru.fasdev.ratex.ui.adapter.epoxy.listSelectCurrency.ListSelectCurrencyController
 import ru.fasdev.ratex.ui.view.fragmentListCurrencyRate.ListCurrencyRateFragment
 import javax.inject.Inject
 import javax.inject.Provider
+
 
 class SelectCurrencyBottomSheet : MvpBottomSheetDialogFragment(), SelectCurrencyView
 {
@@ -54,7 +59,26 @@ class SelectCurrencyBottomSheet : MvpBottomSheetDialogFragment(), SelectCurrency
         selectCurrencyComponent.inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog
+    {
+        val dialog = super.onCreateDialog(savedInstanceState)
+
+        dialog.setOnShowListener { dialog ->
+            val wm = context!!.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val display = wm.defaultDisplay
+
+            val d = dialog as BottomSheetDialog
+            dialog.behavior.peekHeight = (display.height / 1.5f).toInt()
+        }
+
+        return dialog
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?
     {
         binding = SelectCurrencyBottomSheetBinding.inflate(inflater)
 
@@ -63,6 +87,12 @@ class SelectCurrencyBottomSheet : MvpBottomSheetDialogFragment(), SelectCurrency
 
         binding.searchCurrency.doOnTextChanged { text, start, before, count ->
             presenter.searchCurrency(text.toString())
+        }
+
+        binding.currencyList.post {
+            binding.currencyList.layoutParams = binding.currencyList.layoutParams.apply {
+                height = ViewGroup.LayoutParams.MATCH_PARENT
+            }
         }
 
         return binding.root
