1189a131c26691b32dfd6b68beaea1d0a71f3278
Andrey Rednikov <andreyrednikov@gmail.com>
2020-09-25 01:59:43 +0300

    Add testGetRealData in CurrencyRateRepoImplTest

--- data/src/test/java/ru/fasdev/ratex/data/currencyRate/CurrencyRateRepoImplTest.kt
+++ data/src/test/java/ru/fasdev/ratex/data/currencyRate/CurrencyRateRepoImplTest.kt
@@ -1,22 +1,37 @@
 package ru.fasdev.ratex.data.currencyRate
 
+import io.reactivex.Single
+import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.fasdev.ratex.data.retrofit.exchangeRates.ExchangeRateApi
+import ru.fasdev.ratex.domain.currency.boundaries.repo.CurrencyBaseRepo
+import ru.fasdev.ratex.domain.currency.boundaries.repo.CurrencyRateRepo
+import ru.fasdev.ratex.domain.currency.entity.CurrencyDomain
+import ru.fasdev.ratex.domain.currency.entity.RateCurrencyDomain
+import java.util.concurrent.TimeUnit
 
 
 class CurrencyRateRepoImplTest
 {
+    private lateinit var currencyBaseRepo: CurrencyBaseRepo
+
     @Before
     fun setUp()
     {
-
+        currencyBaseRepo = Mockito.mock(CurrencyBaseRepo::class.java)
+        Mockito.`when`(currencyBaseRepo.getBaseCurrency()).thenReturn(Single.just(CurrencyDomain.getInstance("USD")))
     }
-}
 
-/*
-class ExchangeRateRepoImplTest
-{
     @Test
-    fun testRealData()
+    fun testGetRealData()
     {
         var exchangeRateRepo: CurrencyRateRepo
 
@@ -36,12 +51,12 @@ class ExchangeRateRepoImplTest
             .client(okHttpClient)
             .build()
 
-        exchangeRateRepo = ExchangeRateRepoImpl(retrofit.create(ExchangeRateApi::class.java), FlagCdnRepoImpl())
+        exchangeRateRepo = CurrencyRateRepoImpl(retrofit.create(ExchangeRateApi::class.java), currencyBaseRepo, FlagCdnRepoImpl())
         //#endregion
 
         val testObserver: TestObserver<List<RateCurrencyDomain>> = TestObserver()
 
-        exchangeRateRepo.getExchangeRates("USD")
+        exchangeRateRepo.getExchangeRates()
             .subscribe(testObserver)
 
         testObserver
@@ -49,11 +64,4 @@ class ExchangeRateRepoImplTest
             .assertComplete()
             .assertValue { it -> !it.isNullOrEmpty() }
     }
-
-    @Test
-    fun testFakeData()
-    {
-        //TODO: IMPL TEST FOR FAKE DATA
-    }
 }
- */
